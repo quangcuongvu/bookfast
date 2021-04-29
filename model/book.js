d36a7c66307f0faa5306cac177a74bb5aeea8d35
Cường Vũ Quang <cophongsuongbn@gmail.com>
2021-04-29 17:10:23 +0700

    final

--- model/book.js
+++ model/book.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose');
 
-const book = new mongoose.Schema({ 
+const book = new mongoose.Schema({
+    cat_id:{type:Number}, 
     id_book: {type:Number}, 
     name_book: {type:String},
     name_unicode: {type:String},
-    link_book: {type:String},
-    img_book: {type:String},
     price_book: {type:Number},
     list_price: {type:Number},
     discount_rate: {type:Number},
+    link_book: {type:String},
+    img_book: {type:String},
     category: {type:String},
     category_unicode:{type:String},
-    cat_id:{type:Number},
     name_author:{type:String},
+    name_auhtor:{type:String},
     author_unicode:{type:String},
     id_author:{type:Number},
     source:{type:String},
     source_id:{type:String},
     short_description:{type:String},
-    description:{type:String}
-},{collection:'books'});
+    description:{type:String},
+    list_produce_fahasa:{type:Array},
+    list_produce_lazada:{type:Array}
 
+// },{collection:'books_meger2'});
+},{collection:'gopnhom'});
 module.exports=mongoose.model('book', book);
--- routes/index copy.js
+++ routes/index copy.js
@@ -28,7 +28,7 @@ router.get('/', function(req, res, next) {
     .limit(pageOptions.limit)
     .exec(function(err, dulieu) {
       bookModel.count().exec(function (err, count) {
-        var max_page=Math.ceil(count/pageOptions.limit);
+        var max_page=Math.floor(count/pageOptions.limit)-1;
         res.render('index', { title: 'Trang chủ',data:dulieu,currentpage:pageOptions.page,next:next,previous:previous,max_page:max_page });
         
       })
@@ -37,53 +37,93 @@ router.get('/', function(req, res, next) {
 
 
 
-router.get('/category/:category.html', function(req, res, next) {
-  var category=req.params.category;
-  var pageOptions = {
-    page: parseInt(req.query.page, 10) || 1,
-    limit: parseInt(req.query.limit, 10) || 24, 
-  }
-  if (pageOptions.page <0){
-    pageOptions.page=1
-  }
+router.get('/category/*.:cat_id.html', function(req, res, next) {
+  var category=req.params.cat_id;
+  const page= parseInt(req.query.page, 10) || 1;
+  const limit= parseInt(req.query.limit, 10) || 24;
 
-  var next=pageOptions.page +1 ;
-  var previous=pageOptions.page-1;
-  console.log(next);
-  console.log(previous);
+  if (page <0){
+    page=1
+  }
+  const page_next=page+1;
+  const page_previous=page-1;
+  const startIndex=(page-1)*limit;
+  const endIndex=page*limit;
 
-  bookModel.find({category: category})
-    .skip(pageOptions.page * pageOptions.limit)
-    .limit(pageOptions.limit)
-    .exec(function(err, dulieu) {
-      bookModel.find({category: category}).count().exec(function (err, count) {
-        var max_page=Math.ceil(count/pageOptions.limit);
-        res.render('category', { title: 'Danh mục sản phẩm',data:dulieu,currentpage:pageOptions.page,next:next,previous:previous,max_page:max_page });    
-        
-      })
-});
- 
+  bookModel.find({cat_id: category},(error,data)=>{
+    if(error){
+      console.log(error)
+    }else
+    {
+      max_page=Math.ceil(data.length/limit);
+      console.log(max_page)
+      const dulieu=data.slice(startIndex,endIndex)
+      res.render('category', { title: 'Danh mục sản phẩm',data:dulieu,currentpage:page,next:page_next,previous:page_previous,max_page:max_page });   
+    }
+
     
-  
+  });
+
 });
+
+router.get('/author/*.:id_author.html', function(req, res, next) {
+  var author=req.params.id_author;
+  const page= parseInt(req.query.page, 10) || 1;
+  const limit= parseInt(req.query.limit, 10) || 24;
+
+  if (page <0){
+    page=1
+  }
+  const page_next=page+1;
+  const page_previous=page-1;
+  const startIndex=(page-1)*limit;
+  const endIndex=page*limit;
+
+  bookModel.find({id_author: author},(error,data)=>{
+    if(error){
+      console.log(error)
+    }else
+    {
+      max_page=Math.ceil(data.length/limit);
+      console.log(max_page)
+      const dulieu=data.slice(startIndex,endIndex)
+      res.render('author', { title: 'Tác giả',data:dulieu,currentpage:page,next:page_next,previous:page_previous,max_page:max_page });   
+    }
+
+   
+  });
+
+});
+
 router.get('/chi-tiet/*.:id_book.html', function(req, res, next) {
-    var idsanpham = req.params.id_book
+    var idsanpham = req.params.id_book;
     bookModel.findOne({id_book: idsanpham}, function(err, data) {
-      res.render('chi-tiet-san-pham',{title:'Trang chi tiết sản phẩm',data:data});
+      
+      res.render('sp-chi-tiet',{title:'Trang chi tiết sản phẩm',data:data});
     });
   
 });
+
+
 router.get('/search/', function(req, res, next) {
   var searchText =req.query.searchText !== undefined ? req.query.searchText : searchText;
   var searchText=searchText.trim()
+  var searchText=searchText
   var TextUnicode=KhongDau(searchText, ["chuyen", "url"]).toLowerCase()
   console.log(searchText)
   console.log(TextUnicode)
   
-  bookModel.find({name_unicode: {$regex: TextUnicode}})
+  bookModel.find({
+    $or:[
+        {name_unicode: {$regex: TextUnicode}},
+        {author_unicode: {$regex: TextUnicode}},
+        
+        ]})
+    .sort('price_book')
     .limit(24)
     .exec(function(err, dulieu) {
-        res.render('search', { title: 'Tìm kiếm sản phẩm',data:dulieu});    
+      
+        res.render('search', { title: 'Tìm kiếm sản phẩm',data:dulieu});   
       })
 });
 
